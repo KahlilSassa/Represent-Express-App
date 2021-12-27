96483f437f74af7c5f2d0c176048b758330953b6
Kahlil Sassa <kahlilsassa@gmail.co>
2021-12-27 14:30:55 -0600

    Created edit and delete routes

--- backend/controllers/clothing.js
+++ backend/controllers/clothing.js
@@ -3,36 +3,50 @@ const router = express.Router()
 const Clothing = require('../models/clothing')
 
 // HOME ROUTE
-router.get('/', (req,res, next) => {
+router.get('/', (req, res, next) => {
    data = Clothing.find({})
    .then(Clothing => res.json(Clothing))
    .catch(next)
 })
 
-router.get('/new', (req,res) => {
+router.get('/new', (req, res) => {
     res.render('new')
 })
 
-// Show route
-router.get('/:id', (req,res, next) => {
+// SHOW ROUTE
+router.get('/:id', (req, res, next) => {
     Clothing.findById(req.params.id)
     .then((clothing) => res.json(clothing))
     .catch(next)
 })
 
 // POST ROUTE --- create route
-router.post('/', (req,res) => {
-    
-})
-
-// DELETE ROUTE
-router.delete('/:id', (req,res) => {
-
+router.post('/', (req, res, next) => {
+    Clothing.create(req.body)
+    .then(clothing => res.json(clothing))
+    .catch(next)
 })
 
 // EDIT ROUTE
-router.get('/:id/edit', (req,res) => {
+router.get('/:id', (req, res, next) => {
+    Clothing.findById(req.params.id)
+    .then(clothing => res.json(clothing))
+    .catch(next)
+})
 
+router.put('/:id/edit', (req, res, next) => {
+    Clothing.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(clothing => res.json(clothing))
+    .catch(next)
+})
+
+// DELETE ROUTE
+router.delete('/:id', (req, res, next) => {
+    Clothing.findByIdAndDelete(req.params.id)
+    .then(() => res.json({
+        messgae: 'Clothing item was deleted.'
+    }))
+    .catch(next)
 })
 
 module.exports = router
